29c5b43911e589512d50704ce4de6fac59a957fe
sepaev <sepaev@gmail.com>
2021-09-03 01:49:17 +0300

    task1/4

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,27 +1,15 @@
-import { Component } from "react";
-import {
-  FeedbackButton,
-  ItemLi,
-  ListDiv,
-  Section,
-  TitleH1,
-  ItemP,
-} from "./Feedback.styled";
+import { Component, Fragment } from "react";
+import FeedbackOptions from "./FeedbackOptions ";
+import Statistics from "./Statistics";
+import { Section } from "./Section";
 
 export class Feedback extends Component {
   state = { good: 0, neutral: 0, bad: 0 };
 
-  titleFeedback = "Please leave feedback";
-  titleStatistics = "Statistics";
-
-  addFeedback = (e) => {
-    const text = e.target.id;
+  onLeaveFeedback = (e) => {
+    const key = e.target.id;
     this.setState((prevState) => {
-      return {
-        good: text === "good" ? ++prevState.good : prevState.good,
-        neutral: text === "neutral" ? ++prevState.neutral : prevState.neutral,
-        bad: text === "bad" ? ++prevState.bad : prevState.bad,
-      };
+      return { [key]: ++prevState[key] };
     });
   };
 
@@ -30,62 +18,46 @@ export class Feedback extends Component {
     Object.keys(this.state).forEach((key) => {
       counter += this.state[key];
     });
-    // console.log(counter);
     return counter;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = parseInt(this.countTotalFeedback());
+  countPositiveFeedbackPercentage = (total) => {
     const result = (parseInt(this.state.good) * 100) / total;
-    return result ? parseFloat(result).toFixed(0) : 0;
+    return total ? result.toFixed(0) : 0;
+  };
+
+  getStaticChildren = () => {
+    const total = this.countTotalFeedback();
+    return (
+      <Statistics
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+        total={total}
+        positivePercentage={this.countPositiveFeedbackPercentage(total)}
+      />
+    );
+  };
+
+  getFeedbackChildren = () => {
+    return (
+      <FeedbackOptions
+        onLeaveFeedback={this.onLeaveFeedback}
+        options={Object.keys(this.state)}
+      />
+    );
   };
 
   render() {
     return (
-      <Section>
-        <TitleH1>{this.titleFeedback}</TitleH1>
-        <ListDiv>
-          <ItemLi>
-            <FeedbackButton type="button" onClick={this.addFeedback} id="good">
-              Good
-            </FeedbackButton>
-          </ItemLi>
-          <ItemLi>
-            <FeedbackButton
-              type="button"
-              onClick={this.addFeedback}
-              id="neutral"
-            >
-              Neutral
-            </FeedbackButton>
-          </ItemLi>
-          <ItemLi>
-            <FeedbackButton type="button" onClick={this.addFeedback} id="bad">
-              Bad
-            </FeedbackButton>
-          </ItemLi>
-        </ListDiv>
-        <TitleH1>{this.titleStatistics}</TitleH1>
-        <ListDiv>
-          <ItemLi>
-            <ItemP>Good: {this.state.good}</ItemP>
-          </ItemLi>
-          <ItemLi>
-            <ItemP>Neutral: {this.state.neutral}</ItemP>
-          </ItemLi>
-          <ItemLi>
-            <ItemP>Bad: {this.state.bad}</ItemP>
-          </ItemLi>
-          <ItemLi>
-            <ItemP>Total: {this.countTotalFeedback()}</ItemP>
-          </ItemLi>
-          <ItemLi>
-            <ItemP>
-              Positive feedback: {this.countPositiveFeedbackPercentage()}%
-            </ItemP>
-          </ItemLi>
-        </ListDiv>
-      </Section>
+      <Fragment>
+        <Section
+          title="Please leave feedback"
+          children={this.getFeedbackChildren()}
+        />
+        <hr />
+        <Section title="Statistics" children={this.getStaticChildren()} />
+      </Fragment>
     );
   }
 }
--- src/components/Feedback/Feedback.styled.jsx
+++ src/components/Feedback/Feedback.styled.jsx
@@ -1,15 +1,17 @@
 import styled from "@emotion/styled";
 
-export const Section = styled.section`
+export const SectionStyled = styled.section`
   width: 50%;
   margin: auto;
   text-align: center;
 `;
 export const TitleH1 = styled.h1`
   color: green;
+  font-size: 40px;
+  margin-bottom: 50px;
 `;
 
-export const FeedbackButton = styled.button`
+export const FeedbackButtonBtn = styled.button`
   background-color: antiquewhite;
   padding: 10px;
   min-width: 100px;
@@ -25,10 +27,22 @@ export const ListDiv = styled.div`
 
 export const ItemLi = styled.li`
   list-style: none;
+  margin: 5px;
+  background-color: lightgray;
+  border-radius: 20px;
+  filter: drop-shadow(2px 4px 6px black);
 `;
-
+export const ItemButtonLi = styled.li`
+  list-style: none;
+  margin: 5px;
+`;
 export const ItemP = styled.p`
   padding: 10px;
   font-size: 18px;
-  margin: 20px;
+  margin: 0 20px;
+`;
+
+export const NameSpan = styled.span`
+  text-transform: capitalize;
+  pointer-events: none;
 `;
